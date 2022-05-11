5f6da6f71455de5dab837634cdbbb4e2913c7676
Tyler887 <tylermageeshields@gmail.com>
2022-05-11 19:01:42 +0000

    Deploying to deploy from @ EatInstall/Docs@e23cefc37c583bb4428427fd2eee3f6810da9fe0 🚀

---
+++ generate-pages.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifer: GPL-3.0-or-later
+#######################################################################################################
+
+import os, sys
+from glob import glob
+print("------------- Eat Docs Page List Generator -------------")
+if sys.version_info.major < 3:
+  print("Error: You must run Python 3 or later to run this script. (Example: Python 2.7 is not supported. 3.10 is supported.)")
+  exit(1)
+print("VERBose: Checking for documentation entries...")
+files = []
+print("----------- ENTRIES ------------------------------------")
+print("Entries are:")
+for i in glob(os.getcwd() + "/*.html"):
+  print("   * " + os.path.basename(i).replace(".html",""))
+  files.append(i)
+print("----------- LIST BUILD ---------------------------------")
+print("Starting generation...")
+for i in files:
+  with open(os.getcwd() + "/index.html", "a") as f:
+    f.write("\n<li>" + os.path.basename(i).replace(".html", "") + "</li>")
+print("Done, exiting successfully! ✅")
+exit(0)
